16a95cda6746db0d88873fb44e652e2ffe8f7e6f
DefangYan <defang.yan@iqunxing.com>
2019-07-08 09:38:27 +0800

    。。。。merge test1

--- src/ruins/demo/HelloWorld.java
+++ src/ruins/demo/HelloWorld.java
@@ -103,7 +103,7 @@ public class HelloWorld {
         objectObjectHashMap.put(null,"w");
         objectObjectHashMap.put("",21);
 //        objectObjectHashtable.put(null,"q");
-        objectObjectHashtable.put("","we");
+        objectObjectHashtable.put("","weareyoung");
         ArrayList<Object> objects1 = new ArrayList<>();
         objects1.add(0,90);
 

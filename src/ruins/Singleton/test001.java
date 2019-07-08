e94c47439d0da349ebe5d8a222882d497ad4045b
DefangYan <defang.yan@iqunxing.com>
2019-07-08 09:42:58 +0800

    。。。。merge test3

---
+++ src/ruins/Singleton/test001.java
@@ -0,0 +1,11 @@
+package ruins.Singleton;
+
+/**
+ * @author defang.yan
+ * @date 19-7-8 上午9:42
+ */
+public class test001 {
+    public static void main(String[] args) {
+        System.out.println("这是xingxing分支没有的");
+    }
+}
